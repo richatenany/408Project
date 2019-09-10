d033a1122b0ce0a003ac2f556f29c2d9d08436d0
Ashwin <mahesh.ashwin1998@gmail.com>
2019-09-10 13:23:29 -0400

    Added all components and set up file structure, set up server and base dependencies

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardSectionComponent } from './dashboard-section/dashboard-section.component';
+import { TaskInfoComponent } from './task-info/task-info.component';
+import { TaskCardComponent } from './task-card/task-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    HeaderComponent,
+    DashboardSectionComponent,
+    TaskInfoComponent,
+    TaskCardComponent
   ],
   imports: [
     BrowserModule,
