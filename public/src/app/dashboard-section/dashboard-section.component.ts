8c8fada98fab32e6b521d219655c24aa03c5c3f3
Ashwin <mahesh.ashwin1998@gmail.com>
2019-09-16 17:54:29 -0400

    Moved the sections into component

--- public/src/app/dashboard-section/dashboard-section.component.ts
+++ public/src/app/dashboard-section/dashboard-section.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-section',
   templateUrl: './dashboard-section.component.html',
   styleUrls: ['./dashboard-section.component.css']
 })
+// type SECTION_NAME = 'To-Do' | 'In-Progress' | 'DONE';
 export class DashboardSectionComponent implements OnInit {
+  
+  @Input() sectionName;
+  showPlus: boolean;
 
-  constructor() { }
+  constructor() { 
+    this.showPlus = false;
+  }
 
   ngOnInit() {
+    if(this.sectionName === 'To-Do') {
+      this.showPlus = true;
+    }
   }
 
 }
