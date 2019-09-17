ade3a8fb067efe82a89a8624f9a52de73e73e802
Ashwin <mahesh.ashwin1998@gmail.com>
2019-09-17 19:30:56 -0400

    Connected task-info screen

--- public/src/app/dashboard/dashboard.component.ts
+++ public/src/app/dashboard/dashboard.component.ts
@@ -7,16 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   addPressed:boolean;
+  sectionToSwitch: number;
   constructor() {
     this.addPressed = false;
+    this.sectionToSwitch = -1;
    }
 
   ngOnInit() {
   }
-  switchListener(info: {newTab:string, section:number}) {
+  switchListener(info: {newTab:string, section:number, taskID?:number}) {
     const {newTab, section} = info;
     if(newTab === "addTask" && section === 0){
       this.addPressed = true;
+      return;
+    }
+    if(newTab === 'taskInfo' && info.taskID) {
+      const { taskID } = info;
+      this.sectionToSwitch = section;
     }
     console.log("Received event:", info);
   }
