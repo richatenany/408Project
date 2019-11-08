649ae5e3ea268247d786760a7dacf809024bfe57
Richa Tenany <richa.tenany@gmail.com>
2019-11-08 16:05:54 -0500

    asdasd

--- app.js
+++ app.js
@@ -716,7 +716,7 @@ app.all('*', (request, response, next) => {
     return response.sendFile(path.resolve('./public/dist/public/index.html'))
 })
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 app.listen(port, function(){
     console.log("Server is listening on port 8080");
 });
